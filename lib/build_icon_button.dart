0705f8b7ffab94ff53646a82a84a7d1e4c9171b8
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-21 01:59:42 +0100

    comment the code

--- lib/build_icon_button.dart
+++ lib/build_icon_button.dart
@@ -1,6 +1,10 @@
+// Import necessary packages
 import 'package:flutter/material.dart';
+
+// Import constants file for using predefined values
 import 'constants.dart';
 
+// Define a stateless widget for a custom IconButton
 class BuildIconButton extends StatelessWidget {
   const BuildIconButton({
     Key? key,
@@ -8,26 +12,29 @@ class BuildIconButton extends StatelessWidget {
     required this.onPressed,
   }) : super(key: key);
 
+  // Properties to store the icon and callback function
   final IconData icon;
   final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery
-        .of(context)
-        .size;
+    // Get the size of the current screen
+    final Size size = MediaQuery.of(context).size;
+
+    // Return a SizedBox with a specified width and height
     return SizedBox(
       width: size.width > 600 ? 80 : 40,
       height: size.width > 600 ? 62 : 32,
       child: OutlinedButton(
+        // Styling for the OutlinedButton
         style: OutlinedButton.styleFrom(
           padding: EdgeInsets.zero,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(13),
+            borderRadius: BorderRadius.circular(13), // Set border radius
           ),
         ),
-        onPressed: onPressed,
-        child: Icon(icon, color: lightTextColor),
+        onPressed: onPressed, // Callback function when button is pressed
+        child: Icon(icon, color: lightTextColor), // Display the specified icon with a predefined color
       ),
     );
   }
