58f089a7ff6078591e65ed4324753a7b0610f12d
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-22 10:05:53 +0100

    add comments to main_test.dart

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,12 +1,15 @@
+// Importing necessary packages and files
 import 'package:cinnamon_rolls_shop/src/screens/splash_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:cinnamon_rolls_shop/main.dart';
 import 'package:cinnamon_rolls_shop/src/cart_provider.dart';
 
+// The main entry point for the test suite
 void main() {
+  // Widget test for the main app
   testWidgets('Main app widget test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Building the widget tree with ChangeNotifierProvider and MyApp
     await tester.pumpWidget(
       ChangeNotifierProvider(
         create: (context) => CartProvider(),
@@ -14,9 +17,10 @@ void main() {
       ),
     );
 
+    // Waiting for the splash screen delay
     await tester.pump(const Duration(seconds: 3));
 
-    // Verify that the SplashScreen is present.
+    // Verifying that the SplashScreen is present in the widget tree
     expect(find.byType(SplashScreen), findsOneWidget);
   });
 }
