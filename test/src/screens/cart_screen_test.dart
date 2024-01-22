2d3a7b8f8379abf2de84bccfa00c6917769f9adc
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-22 09:52:12 +0100

    add comments to cart_screen_test.dart

--- test/src/screens/cart_screen_test.dart
+++ test/src/screens/cart_screen_test.dart
@@ -1,12 +1,17 @@
+// Importing necessary packages and files
 import 'package:cinnamon_rolls_shop/src/cart_provider.dart';
 import 'package:cinnamon_rolls_shop/src/screens/cart_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 
+// The main entry point for the test suite
 void main() {
+  // Grouping tests related to the 'CartScreen' widget
   group('CartScreen', () {
+    // Test to ensure CartScreen renders correctly
     testWidgets('Renders CartScreen correctly', (WidgetTester tester) async {
+      // Pumping the widget tree with CartScreen wrapped in a ChangeNotifierProvider
       await tester.pumpWidget(
         ChangeNotifierProvider(
           create: (context) => CartProvider(),
@@ -16,12 +21,17 @@ void main() {
         ),
       );
 
+      // Waiting for animations to complete
       await tester.pumpAndSettle();
+
+      // Expecting the 'checkout_button' widget to be present
       expect(find.byKey(const Key('checkout_button')), findsOneWidget);
     });
 
+    // Test to check if pressing 'Proceed to Checkout' button triggers a SnackBar
     testWidgets('Proceed to Checkout button triggers SnackBar',
         (WidgetTester tester) async {
+      // Pumping the widget tree with CartScreen wrapped in a ChangeNotifierProvider
       await tester.pumpWidget(
         ChangeNotifierProvider(
           create: (context) => CartProvider(),
@@ -31,16 +41,21 @@ void main() {
         ),
       );
 
+      // Waiting for animations to complete
       await tester.pumpAndSettle();
 
+      // Tapping the 'checkout_button'
       await tester.tap(find.byKey(const Key('checkout_button')));
       await tester.pump();
 
+      // Expecting the 'cart_snackbar' widget to be present
       expect(find.byKey(const Key('cart_snackbar')), findsOneWidget);
     });
 
+    // Test to check if SnackBar is not present when not triggered
     testWidgets('SnackBar is not present if not triggered',
         (WidgetTester tester) async {
+      // Pumping the widget tree with CartScreen wrapped in a ChangeNotifierProvider
       await tester.pumpWidget(
         ChangeNotifierProvider(
           create: (context) => CartProvider(),
@@ -50,6 +65,7 @@ void main() {
         ),
       );
 
+      // Expecting the 'cart_snackbar' widget to be absent
       expect(find.byKey(const Key('cart_snackbar')), findsNothing);
     });
   });
