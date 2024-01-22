99603f44e39a61c1b400e7f4a056d37dfdb40f12
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-22 09:55:04 +0100

    add comments to splash_screen_test.dart

--- test/src/screens/splash_screen_test.dart
+++ test/src/screens/splash_screen_test.dart
@@ -1,19 +1,23 @@
+// Importing necessary packages and files
 import 'package:cinnamon_rolls_shop/src/cart_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cinnamon_rolls_shop/src/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
 
+// The main entry point for the test suite
 void main() {
+  // Tear down function to ensure that any pending timers are canceled
   tearDown(() {
     // Ensure that any pending timers are canceled
     // This helps avoid issues with pending timers after the widget tree is disposed
     WidgetsBinding.instance?.handleBeginFrame(null);
   });
 
+  // Test to check if the Splash screen navigates to ShopScreen after the delay
   testWidgets('Splash screen navigates to ShopScreen after delay',
       (WidgetTester tester) async {
-    // Build our widget and trigger a frame
+    // Build the widget tree with SplashScreen wrapped in a ChangeNotifierProvider
     await tester.pumpWidget(
       ChangeNotifierProvider(
         create: (context) => CartProvider(),
@@ -22,10 +26,11 @@ void main() {
         ),
       ),
     );
-    // Wait for the splash screen delay
+
+    // Wait for the splash screen delay (3 seconds in this case)
     await tester.pump(const Duration(seconds: 3));
 
-    // Verify that ShopScreen is navigated to
+    // Verify that the ShopScreen is navigated to
     expect(find.byType(SplashScreen), findsOneWidget);
   });
 }
