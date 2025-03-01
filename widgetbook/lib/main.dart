26341c995a2be1230dc9099445df46507efd4a56
GianMen91 <giancarlo.mennillo@gmail.com>
2025-03-02 00:19:51 +0100

    fix provider error

--- widgetbook/lib/main.dart
+++ widgetbook/lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+import 'package:provider/provider.dart';
+import 'package:cinnamon_rolls_shop/src/cart_provider.dart';
 
 // This file does not exist yet,
 // it will be generated in the next step
 import 'main.directories.g.dart';
 
 void main() {
-  runApp(const WidgetbookApp());
+  // Run the app with the CartProvider using ChangeNotifierProvider
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: const WidgetbookApp(),
+    ),
+  );
 }
 
 @widgetbook.App()
