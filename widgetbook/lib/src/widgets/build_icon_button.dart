7082c832b9fdff5f42033470e550962cde3bbc28
GianMen91 <giancarlo.mennillo@gmail.com>
2025-03-01 23:46:26 +0100

    add widgetbook

---
+++ widgetbook/lib/src/widgets/build_icon_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+// Import the widget from your app
+import 'package:cinnamon_rolls_shop/src/widgets/build_icon_button.dart';
+
+@widgetbook.UseCase(name: 'Default', type: BuildIconButton)
+Widget buildCoolButtonUseCase(BuildContext context) {
+  return BuildIconButton(icon: Icons.remove, onPressed: () {  },);
+}
